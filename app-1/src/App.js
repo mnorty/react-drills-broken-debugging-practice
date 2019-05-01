000281e69c9dbb397cdfa5b844ca85bd7503145e
mnorty <mnorty@gmail.com>
2019-05-01 09:36:08 -0600

    app1 done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state= {
+      input:""
+    }
+  }
   handleInput(value) {
-    this.setState({
-      input: value
-    })
+    this.setState({input:value})
   }
 
   render() {
     return (
       <div className="App">
-        <input onChange={(e) => this.handleInput(e)} />
+        <input onChange={(e) => this.handleInput(e.target.value)} type='text' />
         <h1>{this.state.input}</h1>
       </div>
     )
